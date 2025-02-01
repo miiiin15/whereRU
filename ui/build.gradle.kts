4326feb736f3da3ba85b5d18c89b370fd47fa48e
mintae <gg04253@gmail.com>
2025-02-01 12:14:38 +0900

    build: UI 모듈 추가 및 MainActivity 이동

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.miiiin15.whereru"
-    compileSdk = 35
+    namespace = "com.miiiin15.whereru.ui"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.miiiin15.whereru"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -37,15 +33,11 @@ android {
 }
 
 dependencies {
-    implementation(project(":ui"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-
 }
--- ui/src/main/java/com/miiiin15/whereru/ui/main/MainActivity.kt
+++ ui/src/main/java/com/miiiin15/whereru/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.miiiin15.whereru
+package com.miiiin15.whereru.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.miiiin15.whereru.ui.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
