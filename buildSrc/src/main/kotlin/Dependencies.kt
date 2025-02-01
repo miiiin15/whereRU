7f30c622601b55ee9e0848051e27ed8a8acee193
mintae <gg04253@gmail.com>
2025-02-01 17:49:21 +0900

    build: Gradle 빌드 스크립트 모듈화

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,20 @@
+object ApplicationId {
+    val id = "com.miiiin15.whereru"
+}
+
+object Modules {
+    val app = ":app"
+    val ui = ":ui"
+    val common = ":common"
+}
+
+object Releases {
+    val versionCode = 1
+    val versionName = "1.0.0"
+}
+
+object Config {
+    const val compileSdk = 35
+    const val targetSdk = 35
+    const val minSdk = 21
+}
