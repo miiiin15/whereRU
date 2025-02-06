7c0a0a480d1823f1b3f546b543e5590af963f380
mintae <gg04253@gmail.com>
2025-02-06 14:09:27 +0900

    feat: MainActivity dispatchTouchEvent clearFocus 옵션 추가

--- ui/src/main/kotlin/com/miiiin15/whereru/ui/main/MainActivity.kt
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/main/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.miiiin15.whereru.ui.main
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.miiiin15.whereru.ui.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,5 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            currentFocus?.let { view ->
+                val outRect = Rect()
+                view.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    view.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(view.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
