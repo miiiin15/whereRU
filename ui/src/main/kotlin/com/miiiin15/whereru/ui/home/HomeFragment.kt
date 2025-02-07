5b098760184bb391d6de73c2018b4f1758bbbcd6
mintae <gg04253@gmail.com>
2025-02-07 10:45:33 +0900

    feat: 홈 화면 초안 추가

---
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.miiiin15.whereru.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.viewModels
+import com.miiiin15.whereru.presentation.viewmodel.HomeViewModel
+import com.miiiin15.whereru.ui.R
+import com.miiiin15.whereru.ui.base.BaseFragment
+import com.miiiin15.whereru.ui.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel,HomeViewModel.Event>(R.layout.fragment_home) {
+    override val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    showCustomAlert("앱을 종료하시겠습니까?") {
+                        requireActivity().finish()
+                    }
+                }
+            }
+        )
+
+    }
+
+    override fun handleEvent(event: HomeViewModel.Event) {
+    }
+}
