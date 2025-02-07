b0ff7ae6c544b63043553d4e2a50b2a08f971c74
mintae <gg04253@gmail.com>
2025-02-07 12:56:41 +0900

    feat: local 모듈 추가

---
+++ local/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("module.android")
+}
+android {
+    namespace = "com.miiiin15.whereru.local"
+}
+dependencies {
+    implementation(projects.data)
+    implementation(projects.common)
+
+    implementation(libs.gson)
+    implementation(libs.androidx.room)
+    kapt(libs.androidx.room.compiler)
+}
