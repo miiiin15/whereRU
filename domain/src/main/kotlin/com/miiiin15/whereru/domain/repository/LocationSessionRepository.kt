3c0b212d12937e1508f435ea841dfad490c209b8
mintae <gg04253@gmail.com>
2025-02-08 18:40:19 +0900

    comment: 주석 수정 및 불필요한 import 제거

--- domain/src/main/kotlin/com/miiiin15/whereru/domain/repository/LocationSessionRepository.kt
+++ domain/src/main/kotlin/com/miiiin15/whereru/domain/repository/LocationSessionRepository.kt
@@ -1,12 +1,9 @@
 package com.miiiin15.whereru.domain.repository
 
 import com.miiiin15.whereru.data_resource.DataResource
-import com.miiiin15.whereru.domain.model.LocationData
 import kotlinx.coroutines.flow.Flow
 
-// 위치 공유 세션 관련
 interface LocationSessionRepository {
     fun createSession(sessionId: String, hostId: String): Flow<DataResource<Unit>>
-//  fun endSession(sessionId: String): Result<Unit>
-//  fun observeSession(sessionId: String): Flow<Map<String, LocationData>>
+//  TODO : fun endSession(sessionId: String): Flow<DataResource<Unit>>
 }
