fc5e9ebc5826d9661dc8330e910f8c02e54e2a5f
mintae <gg04253@gmail.com>
2025-02-09 15:02:41 +0900

    feat: AuthViewModel 마지막 로그인 시간 갱신, 프로필 저장 기능 연결

--- presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/AuthViewModel.kt
+++ presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/AuthViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import com.miiiin15.whereru.common.utils.AuthSessionManager
 import com.miiiin15.whereru.domain.usecase.LoginUseCase
 import com.miiiin15.whereru.domain.usecase.RegisterUserUseCase
+import com.miiiin15.whereru.domain.usecase.SetProfileUseCase
+import com.miiiin15.whereru.domain.usecase.UpdateLastLoginUseCase
 import com.miiiin15.whereru.local.model.AuthInfoModel
 import com.miiiin15.whereru.local.pref.PrefUtil
 import com.miiiin15.whereru.presentation.base.BaseViewModel
@@ -18,6 +20,8 @@ import javax.inject.Inject
 class AuthViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     private val registerUserUseCase: RegisterUserUseCase,
+    private val setProfileUseCase: SetProfileUseCase,
+    private val updateLastLoginUseCase: UpdateLastLoginUseCase,
     private val authSessionManager: AuthSessionManager,
     private val prefUtil: PrefUtil
 ) : BaseViewModel<AuthViewModel.Event>() {
@@ -47,6 +51,7 @@ class AuthViewModel @Inject constructor(
         launch {
             loginUseCase(email.value!!, password.value!!)
                 .collectDataResource({
+                    updateLastLoginTime(it)
                     _authState.value = true
                     authSessionManager.login(it)
                     if (prefUtil.authInfoModel == null) prefUtil.authInfoModel =
@@ -59,17 +64,35 @@ class AuthViewModel @Inject constructor(
         launch {
             registerUserUseCase(email.value!!, password.value!!)
                 .collectDataResource({
+                    setInitialProfile(it)
                     login()
                 })
         }
     }
 
+    // 프로필 기본값 세팅
+    private suspend fun setInitialProfile(uid: String) {
+        setProfileUseCase(
+            userId = uid,
+            profileImageUrl = "",
+            nickname = "유저_${uid.substring(0, 8)}",
+            lastLoginAt = System.currentTimeMillis()
+        ).await()
+    }
+
+    // 로그인 시간 갱신
+    private suspend fun updateLastLoginTime(uid: String) {
+        updateLastLoginUseCase(uid, System.currentTimeMillis()).await()
+    }
+
+    // 자동 로그인
     private fun autoLogin() {
         prefUtil.authInfoModel?.let {
             launch {
                 loginUseCase(it.email, it.password)
                     .collectDataResource(
                         onSuccess = {
+                            updateLastLoginTime(it)
                             _authState.value = true
                             authSessionManager.login(it)
                         },
