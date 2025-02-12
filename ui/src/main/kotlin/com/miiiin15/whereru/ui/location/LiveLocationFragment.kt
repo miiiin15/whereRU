6184d734f228d33187bb5798ae323f8bc7928523
mintae <gg04253@gmail.com>
2025-02-12 17:31:22 +0900

    refactor: 마커 관련 요소 MarkerManager로 분리

--- ui/src/main/kotlin/com/miiiin15/whereru/ui/location/LiveLocationFragment.kt
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/location/LiveLocationFragment.kt
@@ -1,9 +1,6 @@
 package com.miiiin15.whereru.ui.location
 
-import android.animation.ValueAnimator
 import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -12,11 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.miiiin15.whereru.presentation.model.LiveLocationUserUiModel
 import com.miiiin15.whereru.presentation.viewmodel.LiveLocationViewModel
 import com.miiiin15.whereru.ui.R
 import com.miiiin15.whereru.ui.base.BaseFragment
@@ -31,15 +24,46 @@ class LiveLocationFragment :
     override val viewModel: LiveLocationViewModel by viewModels()
 
     private var mMap: GoogleMap? = null
-    private val markers = mutableMapOf<String, Marker>()
+    private lateinit var markerManager: MarkerManager
     private var isCameraMoved = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        markerManager = MarkerManager(mMap, resources)
+
+        mMap!!.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap!!.isMyLocationEnabled = true
+
+        with(mMap!!.uiSettings) {
+            isZoomControlsEnabled = true
+            isCompassEnabled = false
+            isMyLocationButtonEnabled = true
+            isMapToolbarEnabled = true
+            isScrollGesturesEnabled = true
+            isZoomGesturesEnabled = true
+            isRotateGesturesEnabled = false
+        }
+
+        // 초기 카메라 위치 설정
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.556, 126.97), 10f))
+
+        // ViewModel 관찰자 설정
+        setupViewModelObservers()
+    }
+
+    private fun setupViewModelObservers() {
         viewModel {
             users observe { users ->
-                updateMarkers(users)
+                markerManager.updateMarkers(users)
                 binding.liveLocationMarkerCountText.text = "${users.size}"
             }
             myLocation observe {
@@ -75,13 +99,7 @@ class LiveLocationFragment :
             liveLocationReceiveSwitch.setOnCheckedChangeListener { _, isChecked ->
                 viewModel.setObserveSessionState(isChecked)
             }
-
-
         }
-
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 
     override fun onDestroy() {
@@ -95,81 +113,9 @@ class LiveLocationFragment :
         viewModel.currentMyLocation()
     }
 
-    private fun updateMarkers(users: Map<String, LiveLocationUserUiModel>) {
-        val newMarkers = mutableMapOf<String, Marker>()
-
-        // 기존 마커와 새로운 유저 목록을 비교하여 갱신
-        users.forEach { (userId, user) ->
-            val newPosition = LatLng(user.location.latitude, user.location.longitude)
-            if (markers.containsKey(userId)) {
-                val marker = markers[userId]
-                // 기존 마커 위치가 변경된 경우에만 위치 갱신
-                if (marker?.position != newPosition) {
-                    animateMarkerTo(marker, newPosition)
-                }
-                newMarkers[userId] = marker!!
-            } else {
-                // 새로운 마커 추가
-                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.profile_image_default)
-                val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false) // 원하는 크기로 조절
-                val marker = mMap?.addMarker(
-                    MarkerOptions()
-                        .position(newPosition)
-                        .title(user.nickname)
-                        .icon(BitmapDescriptorFactory.fromBitmap(scaledBitmap))
-                )
-                if (marker != null) {
-                    newMarkers[userId] = marker
-                }
-            }
-        }
-
-        // 기존 마커 중 새로운 목록에 없는 마커 제거
-        markers.keys.subtract(newMarkers.keys).forEach { userId ->
-            markers[userId]?.remove()
-        }
-
-        // 마커 목록 갱신
-        markers.clear()
-        markers.putAll(newMarkers)
-    }
-
-    private fun animateMarkerTo(marker: Marker?, newPosition: LatLng) {
-        val startPosition = marker?.position
-        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
-        valueAnimator.duration = 1000 // 애니메이션 지속 시간 (밀리초)
-        valueAnimator.addUpdateListener { animation ->
-            val v = animation.animatedFraction
-            val lng = v * newPosition.longitude + (1 - v) * startPosition!!.longitude
-            val lat = v * newPosition.latitude + (1 - v) * startPosition.latitude
-            marker?.position = LatLng(lat, lng)
-        }
-        valueAnimator.start()
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        mMap!!.mapType = GoogleMap.MAP_TYPE_NORMAL
-        mMap!!.isMyLocationEnabled = true
-
-        with(mMap!!.uiSettings) {
-            isZoomControlsEnabled = true
-            isCompassEnabled = false
-            isMyLocationButtonEnabled = true
-            isMapToolbarEnabled = true
-            isScrollGesturesEnabled = true
-            isZoomGesturesEnabled = true
-            isRotateGesturesEnabled = false
-        }
-
-        // 초기 카메라 위치 설정
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.556, 126.97), 10f))
-    }
-
     override fun handleEvent(event: LiveLocationViewModel.Event) {
         when (event) {
-            is LiveLocationViewModel.Event.UsersUpdated -> updateMarkers(event.users)
+            is LiveLocationViewModel.Event.UsersUpdated -> markerManager.updateMarkers(event.users)
         }
     }
 }
---
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/location/MarkerManager.kt
@@ -0,0 +1,67 @@
+package com.miiiin15.whereru.ui.location
+
+import android.animation.ValueAnimator
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.miiiin15.whereru.presentation.model.LiveLocationUserUiModel
+import com.miiiin15.whereru.ui.R
+
+class MarkerManager(
+    private val mMap: GoogleMap?,
+    private val resources: Resources
+) {
+    private val markers = mutableMapOf<String, Marker>()
+
+    fun updateMarkers(users: Map<String, LiveLocationUserUiModel>) {
+        val newMarkers = mutableMapOf<String, Marker>()
+
+        users.forEach { (userId, user) ->
+            val newPosition = LatLng(user.location.latitude, user.location.longitude)
+            if (markers.containsKey(userId)) {
+                val marker = markers[userId]
+                if (marker?.position != newPosition) {
+                    animateMarkerTo(marker, newPosition)
+                }
+                newMarkers[userId] = marker!!
+            } else {
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.profile_image_default)
+                val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false)
+                val marker = mMap?.addMarker(
+                    MarkerOptions()
+                        .position(newPosition)
+                        .title(user.nickname)
+                        .icon(BitmapDescriptorFactory.fromBitmap(scaledBitmap))
+                )
+                if (marker != null) {
+                    newMarkers[userId] = marker
+                }
+            }
+        }
+
+        markers.keys.subtract(newMarkers.keys).forEach { userId ->
+            markers[userId]?.remove()
+        }
+
+        markers.clear()
+        markers.putAll(newMarkers)
+    }
+
+    private fun animateMarkerTo(marker: Marker?, newPosition: LatLng) {
+        val startPosition = marker?.position
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.duration = 1000
+        valueAnimator.addUpdateListener { animation ->
+            val v = animation.animatedFraction
+            val lng = v * newPosition.longitude + (1 - v) * startPosition!!.longitude
+            val lat = v * newPosition.latitude + (1 - v) * startPosition.latitude
+            marker?.position = LatLng(lat, lng)
+        }
+        valueAnimator.start()
+    }
+}
