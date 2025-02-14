7e98894939a91b78ca706338cc1ac3b1f3735aeb
mintae <gg04253@gmail.com>
2025-02-14 14:30:28 +0900

    feat: session, user ItemClickListener 인터페이스 추가

---
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/home/OnSessionItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.miiiin15.whereru.ui.home
+
+import com.miiiin15.whereru.presentation.model.LocationSessionUiModel
+
+interface OnSessionItemClickListener {
+    fun onSessionItemClick(session: LocationSessionUiModel)
+}
---
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/home/OnUserItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.miiiin15.whereru.ui.home
+
+import com.miiiin15.whereru.presentation.model.UserUiModel
+
+interface OnUserItemClickListener {
+    fun onUserItemClick(user: UserUiModel)
+}
