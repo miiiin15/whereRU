6e6fedbcc89a923925792926864053e7d0abbfb7
mintae <gg04253@gmail.com>
2025-02-15 14:24:31 +0900

    feat: 알림 권한 확인 로직 추가

--- common/src/main/java/com/miiiin15/whereru/common/utils/PermissionManager.kt
+++ common/src/main/java/com/miiiin15/whereru/common/utils/PermissionManager.kt
@@ -1,7 +1,12 @@
 package com.miiiin15.whereru.common.utils
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.ActivityResultLauncher
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 object PermissionManager {
@@ -52,4 +57,18 @@ object PermissionManager {
         }
         callback?.invoke()
     }
+
+    fun askNotificationPermission(
+        context: Context,
+        requestPermissionLauncher: ActivityResultLauncher<String>
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
 }
--- ui/src/main/kotlin/com/miiiin15/whereru/ui/main/MainActivity.kt
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/main/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.miiiin15.whereru.ui.main
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Rect
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.miiiin15.whereru.common.utils.PermissionManager
 import com.miiiin15.whereru.ui.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PermissionManager.askNotificationPermission(this, requestPermissionLauncher)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
@@ -31,4 +39,22 @@ class MainActivity : AppCompatActivity() {
         }
         return super.dispatchTouchEvent(ev)
     }
+
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (!isGranted) {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", packageName, null)
+            }
+            Toast.makeText(
+                this,
+                "알림을 받기 위해 알림 권한을 허용해주세요.",
+                Toast.LENGTH_LONG
+            ).show()
+            startActivity(intent)
+        }
+    }
+
 }
