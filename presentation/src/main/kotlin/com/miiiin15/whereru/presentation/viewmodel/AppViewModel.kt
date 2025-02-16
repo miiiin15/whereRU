df9dcd83aa2ac0a5955fb994cbebedf9275c7ae0
mintae <gg04253@gmail.com>
2025-02-16 11:04:22 +0900

    feat: 스플래시 화면 토큰 조회 기능 연결

--- presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/AppViewModel.kt
+++ presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/AppViewModel.kt
@@ -1,6 +1,7 @@
 package com.miiiin15.whereru.presentation.viewmodel
 
 import com.miiiin15.whereru.domain.session.AuthSessionManager
+import com.miiiin15.whereru.domain.usecase.fcm.GetFCMTokenUseCase
 import com.miiiin15.whereru.presentation.base.BaseViewModel
 import com.miiiin15.whereru.presentation.base.ViewEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,11 +9,19 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AppViewModel @Inject constructor(
+    private val fcmTokenUseCase: GetFCMTokenUseCase,
     private val authSessionManager: AuthSessionManager,
 ) : BaseViewModel<AppViewModel.Event>() {
 
     init {
-        authSessionManager.clear()
+        launch {
+            authSessionManager.clear()
+            getFCMToken()
+        }
+    }
+
+    suspend fun getFCMToken() {
+        val token = fcmTokenUseCase().await()
     }
 
     sealed class Event : ViewEvent
