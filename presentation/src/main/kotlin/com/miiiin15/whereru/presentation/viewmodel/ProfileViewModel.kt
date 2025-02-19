f34b71c7be8c02d8551319246d3d8a83bc79790b
mintae <gg04253@gmail.com>
2025-02-19 16:45:38 +0900

    feat: 프로필 설정 화면 구현

--- presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/ProfileViewModel.kt
+++ presentation/src/main/kotlin/com/miiiin15/whereru/presentation/viewmodel/ProfileViewModel.kt
@@ -1,14 +1,66 @@
 package com.miiiin15.whereru.presentation.viewmodel
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import com.miiiin15.whereru.data_resource.mapDataResource
+import com.miiiin15.whereru.domain.session.AuthSessionManager
+import com.miiiin15.whereru.domain.usecase.profile.GetProfileUseCase
+import com.miiiin15.whereru.domain.usecase.profile.SetProfileUseCase
 import com.miiiin15.whereru.presentation.base.BaseViewModel
 import com.miiiin15.whereru.presentation.base.ViewEvent
+import com.miiiin15.whereru.presentation.model.UserUiModel
+import com.miiiin15.whereru.presentation.model.toPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    // TODO : UseCase 추가
+    private val setProfileUseCase: SetProfileUseCase,
+    private val getProfileUseCase: GetProfileUseCase,
+    private val authSessionManager: AuthSessionManager,
 ) : BaseViewModel<ProfileViewModel.Event>() {
+
+    private val _selectedColor = MutableStateFlow<Int?>(null)
+    val selectedColor = _selectedColor.asStateFlow()
+
+    private val _myProfile = MutableStateFlow<UserUiModel?>(null)
+    val myProfile = _myProfile.asStateFlow()
+
+    val nickname = MutableLiveData("")
+
+    init {
+        fetchProfile()
+    }
+
+    fun setSelectedColor(color: Int) {
+        _selectedColor.value = color
+    }
+
+    private fun fetchProfile() = launch {
+        authSessionManager.uid?.let { uid ->
+            getProfileUseCase(uid)
+                .mapDataResource { it.toPresentation() }
+                .collectDataResource({ profile ->
+                    _myProfile.value = profile
+                    nickname.value = profile.nickname
+                })
+        }
+    }
+
+    fun setProfile(callback: (() -> Unit)) = launch {
+        val uid = authSessionManager.uid!!
+        setProfileUseCase(
+            userId = uid,
+            nickname = nickname.value.takeIf { !it.isNullOrBlank() } ?: "유저_${uid.substring(0, 6)}",
+            profileImageUrl = selectedColor.value.toString(),
+            lastLoginAt = System.currentTimeMillis(),
+            fcmToken = myProfile.value?.fcmToken
+        ).collectDataResource({
+            callback.invoke()
+        })
+    }
 
     sealed class Event : ViewEvent
 }
---
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/profile/ProfileFragment.kt
@@ -0,0 +1,78 @@
+package com.miiiin15.whereru.ui.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.miiiin15.whereru.presentation.viewmodel.ProfileViewModel
+import com.miiiin15.whereru.ui.R
+import com.miiiin15.whereru.ui.base.BaseFragment
+import com.miiiin15.whereru.ui.databinding.FragmentProfileBinding
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class ProfileFragment :
+    BaseFragment<FragmentProfileBinding, ProfileViewModel, ProfileViewModel.Event>(
+        R.layout.fragment_profile
+    ) {
+    override val viewModel: ProfileViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel {
+            selectedColor observe { color ->
+                binding.profileColorPreview.setBackgroundColor(color)
+            }
+
+            myProfile observe { profile ->
+                binding.profileNicknameInput.setText(profile.nickname)
+                setSelectedColor(
+                    profile.profileImageUrl?.toIntOrNull() ?: resources.getColor(R.color.gray1)
+                )
+            }
+        }
+        binding {
+            vm = viewModel
+
+            profileCloseButton.setOnClickListener {
+                requireActivity().onBackPressedDispatcher.onBackPressed()
+            }
+
+            profileColorPreview.setOnClickListener {
+                showColorPickerDialog()
+            }
+
+            profileColorPickerButton.setOnClickListener {
+                showColorPickerDialog()
+            }
+
+            profileConfirmButton.setOnClickListener {
+                viewModel.setProfile {
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+    }
+
+    private fun showColorPickerDialog() {
+        ColorPickerDialog.Builder(requireContext())
+            .setTitle("컬러 선택")
+            .setPreferenceName("ColorPickerDialog")
+            .setPositiveButton("확인", ColorEnvelopeListener { envelope, _ ->
+                val color = envelope.color // ← 진짜 Int값 (ARGB)
+                viewModel.setSelectedColor(color)
+            })
+            .setNegativeButton("취소") { dialog, _ -> dialog.dismiss() }
+            .attachAlphaSlideBar(true)
+            .attachBrightnessSlideBar(true) // 밝기 슬라이더 추가
+            .setBottomSpace(12) // 하단 여백
+            .show()
+    }
+
+    override fun handleEvent(event: ProfileViewModel.Event) {
+
+    }
+}
