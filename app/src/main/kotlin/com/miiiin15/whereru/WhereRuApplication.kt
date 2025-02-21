a3aaaf3e720b995061dd061638beac8c9a1ac16a
mintae <gg04253@gmail.com>
2025-02-21 15:47:44 +0900

    fix: NotificationChannel 생성 메서드 복구 및 channel_description 수정

--- app/src/main/kotlin/com/miiiin15/whereru/WhereRuApplication.kt
+++ app/src/main/kotlin/com/miiiin15/whereru/WhereRuApplication.kt
@@ -1,6 +1,9 @@
 package com.miiiin15.whereru
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.annotation.CallSuper
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,6 +14,23 @@ class WhereRuApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                getString(R.string.channel_id), // 채널 ID
+                getString(R.string.app_name), // 채널 이름
+                NotificationManager.IMPORTANCE_HIGH  // 중요도
+            ).apply {
+                description = getString(R.string.channel_description)
+            }
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 
     companion object {
