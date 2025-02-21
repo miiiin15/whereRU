e0e99195ba28d01841ac7698dcc8e7cb09cc9366
mintae <gg04253@gmail.com>
2025-02-21 11:38:42 +0900

    refactor: 신규, 이탈 유저 관련 상태 변화 콜백 추가

--- ui/src/main/kotlin/com/miiiin15/whereru/ui/location/MarkerManager.kt
+++ ui/src/main/kotlin/com/miiiin15/whereru/ui/location/MarkerManager.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.miiiin15.whereru.presentation.model.LiveLocationUserUiModel
+import com.miiiin15.whereru.presentation.model.LocationUiModel
 import com.miiiin15.whereru.ui.R
 
 class MarkerManager(
@@ -19,8 +20,12 @@ class MarkerManager(
     private val resources: Resources
 ) {
     private val markers = mutableMapOf<String, Marker>()
-    var removedUserNickname: String? = null
-    var newUserNickname: String? = null
+
+    var onUserStateChanged: ((removedUser: LiveLocationUserUiModel?, newUser: LiveLocationUserUiModel?) -> Unit)? =
+        null
+
+    private var _removedUser: LiveLocationUserUiModel? = null
+    private var _newUser: LiveLocationUserUiModel? = null
 
     fun getMarkerPosition(userId: String?): LatLng? {
         if (userId == null) return null
@@ -34,16 +39,9 @@ class MarkerManager(
         users.forEach { (userId, user) ->
             val newPosition = LatLng(user.location.latitude, user.location.longitude)
             val newTime = user.location.timestamp
-            if (markers.containsKey(userId)) { // 좌표가 동일한 기존 마커가 있는 경우
-                val marker = markers[userId]
-                if (marker?.position != newPosition) {
-                    animateMarkerTo(marker, newPosition)
-                }
-                marker?.snippet = newTime
-                newMarkers[userId] = marker!!
-            } else { // 좌표가 다른 새로운 마커인 경우
 
-                newUserNickname = user.nickname
+            if (!markers.containsKey(userId)) {  // uid 가 다르면 새로 추가
+                _newUser = user
 
                 val bitmap =
                     BitmapFactory.decodeResource(resources, R.drawable.profile_image_default)
@@ -73,18 +71,41 @@ class MarkerManager(
                     marker.tag = user.userId
                     newMarkers[userId] = marker
                 }
+            } else { //  uid 가 동일하면 업데이트
+                val marker = markers[userId]
+                if (marker?.position != newPosition) {
+                    animateMarkerTo(marker, newPosition)
+                }
+                marker?.snippet = newTime
+                newMarkers[userId] = marker!!
             }
         }
 
+        // 기존 마커 목록중 사라진 마커 후처리
         markers.keys.subtract(newMarkers.keys).forEach { userId ->
-            removedUserNickname = markers[userId]?.title
+            _removedUser =
+                LiveLocationUserUiModel(
+                    userId = userId,
+                    nickname = markers[userId]?.title ?: "",
+                    profileImageUrl = null,
+                    location = LocationUiModel(0.0, 0.0, "")
+                )
             markers[userId]?.remove()
         }
 
+        // 기존 마커 목록을 새로운 마커 목록으로 대체
         markers.clear()
         markers.putAll(newMarkers)
+
+        // 상태 변화 콜백 호출
+        onUserStateChanged?.invoke(_removedUser, _newUser)
+
+        // 상태 초기화
+        _removedUser = null
+        _newUser = null
     }
 
+    // 애니메이션을 사용하여 마커를 새로운 위치로 이동
     private fun animateMarkerTo(marker: Marker?, newPosition: LatLng) {
         val startPosition = marker?.position
         val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
